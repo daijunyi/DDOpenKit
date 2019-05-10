e4276a219b55b488b80035d4ec29990699bf6037
“红领巾” <812297736@qq.com>
2019-05-10 14:46:26 +0800

    修改了category 中对字体的调整问题

--- DDOpenKit.podspec
+++ DDOpenKit.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "DDOpenKit"
-  s.version      = "1.1.1"
+  s.version      = "1.1.2"
   s.summary      = "A ios tool library"
   s.homepage     = "https://github.com/daijunyi/DDOpenKit"
   s.license      = "MIT"
--- DDOpenKit/Category/UITextField+Runtime.m
+++ DDOpenKit/Category/UITextField+Runtime.m
@@ -12,19 +12,19 @@
 
 @implementation UITextField (Runtime)
 
-+ (void)load{
-    Method method1 = class_getInstanceMethod([self class], @selector(initWithCoder:));
-    Method method2 = class_getInstanceMethod([self class], @selector(runtimeInitWithCoder:));
-    method_exchangeImplementations(method1, method2);
-}
-
-- (instancetype)runtimeInitWithCoder:(NSCoder *)aDecoder{
-    [self runtimeInitWithCoder:aDecoder];
-    if (self) {
-        NSString *familyName = self.font.familyName;
-        self.font = [UIFont fontWithName:familyName size:[UIFont AppFloatFromFloat:self.font.pointSize]];
-    }
-    return self;
-}
+//+ (void)load{
+//    Method method1 = class_getInstanceMethod([self class], @selector(initWithCoder:));
+//    Method method2 = class_getInstanceMethod([self class], @selector(runtimeInitWithCoder:));
+//    method_exchangeImplementations(method1, method2);
+//}
+//
+//- (instancetype)runtimeInitWithCoder:(NSCoder *)aDecoder{
+//    [self runtimeInitWithCoder:aDecoder];
+//    if (self) {
+//        NSString *familyName = self.font.familyName;
+//        self.font = [UIFont fontWithName:familyName size:[UIFont AppFloatFromFloat:self.font.pointSize]];
+//    }
+//    return self;
+//}
 
 @end
